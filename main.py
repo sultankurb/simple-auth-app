1a1b2c56437131831783f900577fc673f0a2bbca
sultan <sultankurbanov302@gmail.com>
2025-04-12 23:12:49 +0500

    add ruff and fix it all

--- main.py
+++ main.py
@@ -1,14 +1,31 @@
-from fastapi import FastAPI
 import uvicorn
-
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(
     title='This service for manage users',
     debug=False,
-    version="0.1"
+    version="0.1",
+        contact={
+        "name": "Sultan Kurbanov",
+        "email": "sultan_kurbanov00.com",
+    }
     
 )
+origins = [
+    "http://localhost:3000/",
+    "http://localhost:80/",
+    "http://localhost:8080/"
+]
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PATCH", "PUT", "DELETE"],
+    allow_headers=["*"],
+
+)
 
 if __name__ == "__main__":
     uvicorn.run(app="main:app", reload=True)
--- src/settings.py
+++ src/settings.py
@@ -1,18 +1,28 @@
-from pydantic_settings import BaseSettings, SettingsConfigDict
-from pydantic import EmailStr
 from pathlib import Path
 
+from pydantic import BaseModel, EmailStr
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 BASE_DIR = Path(__file__).parent.parent
+
+
+class JWTModel(BaseModel):
+    public_key_path: Path = BASE_DIR / "certificates" / "public-key.pem"
+    private_key_path: Path = BASE_DIR / "certificates" / "private-key.pem"
+    access_token_lifetime: int = 15
+    refresh_token_lifetime: int = 30
+    algorithm: str = "RS256"
 
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
         env_file=f"{BASE_DIR}/.env"
     )
+    api_prefix: str = "/api/v1"
     DB_URL: str = None
     ADMIN_PASSWORD: str = None
     ADMIN_EMAIL: EmailStr = None
+    auth: JWTModel = JWTModel()
 
 
 def get_settings() -> Settings:
