70178444c4c52707e5eb82189b56c7e4abe149ba
Sultan <sultankurbanov302@gmail.com>
2025-04-13 17:15:25 +0500

    add base auth to service

--- src/routers/main_router.py
+++ src/routers/main_router.py
@@ -1,7 +1,11 @@
 from fastapi import APIRouter
 
+from src.routers.admin.router import admin
+from src.routers.users.router import users
 from src.settings import settings
 
 api_routers = APIRouter(
     prefix=settings.api_prefix
 )
+api_routers.include_router(router=users)
+api_routers.include_router(router=admin)
